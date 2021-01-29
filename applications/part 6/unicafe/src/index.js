530c65ce766cbcb3cf1593466ff008405bffb71b
eerojala <eero.ojala@helsinki.fi>
2021-01-29 16:33:10 +0200

    6.2 done

--- applications/part 6/unicafe/src/index.js
+++ applications/part 6/unicafe/src/index.js
@@ -1,51 +1,30 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 
 import counterReducer from './reducers/counterReducer'
 
-import Button from './components/Button'
 import Statistics from './components/Statistics'
 
 const store = createStore(counterReducer)
 
-const App = (props) => {
-  // const [good, setGood] = useState(0)
-  // const [neutral, setNeutral] = useState(0)
-  // const [bad, setBad] = useState(0)
-
-  // const goodFeedback = () => {
-  //   setGood(good + 1)
-  // }
-
-  // const neutralFeedback = () => {
-  //   setNeutral(neutral + 1)
-  // }
-
-  // const badFeedback = () => {
-  //   setBad(bad + 1)
-  // }
-
-  // const buttonPresses = {
-  //   good: good,
-  //   neutral: neutral,
-  //   bad: bad
-  // }
-
+const App = () => {
   return (
     <div>
       <div>
         <h1>Give feedback</h1>
-        <button>Good</button>
-        <button>Neutral</button>
-        <button>Bad</button>
+        <button onClick={() => store.dispatch({ type: 'GOOD' })}>Good</button>
+        <button onClick={() => store.dispatch({ type: 'NEUTRAL' })}>Neutral</button>
+        <button onClick={() => store.dispatch({ type: 'BAD' })}>Bad</button>
       </div>
       <Statistics buttonPresses={store.getState()} />
     </div>
   )
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+renderApp()
+store.subscribe(renderApp)
